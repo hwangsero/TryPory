517499996bdc1056df9612f1a6b1b8c5167a734c
hacho <hacho_2011@naver.com>
2018-12-20 17:08:53 +0900

    mypage

--- src/main/webapp/assets/js/mypage.js
+++ src/main/webapp/assets/js/mypage.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	//open popup
-	$('.cd-popup-trigger').on('click', function(event){
+	$('.profileImgBtn').on('click', function(event){
 		event.preventDefault();
 		$('.cd-popup').addClass('is-visible');
 	});
@@ -16,6 +16,29 @@ jQuery(document).ready(function($){
 	$(document).keyup(function(event){
     	if(event.which=='27'){
     		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+
+jQuery(document).ready(function($){
+	//open popup
+	$('.backImgBtn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup02').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup02').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup02').removeClass('is-visible');
 	    }
     });
 });
