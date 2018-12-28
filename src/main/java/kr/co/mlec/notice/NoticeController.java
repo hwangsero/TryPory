0852369dc7d46a22fc7cfda4e5991aea6cacbcf2
황세로 <hsro2228@naver.com>
2018-12-28 14:45:41 +0900

    페이징

--- src/main/java/kr/co/mlec/notice/NoticeController.java
+++ src/main/java/kr/co/mlec/notice/NoticeController.java
@@ -2,7 +2,9 @@ package kr.co.mlec.notice;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,12 +12,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class NoticeController {
 
+	@Autowired
 	private NoticeService noticeService;
 	
 	@RequestMapping("/notice/{pageNo}")
-	public ModelAndView noticeList(@PathVariable("pageNo") int pageNo) {
-		
+	public ModelAndView noticeList(@PathVariable("pageNo") int pageNo,Model model) {
 		ModelAndView mav = new ModelAndView();
+		model.addAttribute(pageNo);
 		List<NoticeVO> notice =  noticeService.selectAllNotice(pageNo);
 		
 		mav.addObject("notice", notice);
