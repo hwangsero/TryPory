1a1d51c7d327ea883e156e2aeee2c0d8f16b62c3
hacho <hacho_2011@naver.com>
2019-01-02 18:46:26 +0900

    mypage

--- src/main/java/kr/co/mlec/mypage/MyPageDAO.java
+++ src/main/java/kr/co/mlec/mypage/MyPageDAO.java
@@ -1,25 +1,27 @@
-package kr.co.mlec.mypage;
-
-import java.util.List;
-import java.util.Map;
-
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import kr.co.mlec.notice.NoticeVO;
-
-public class MyPageDAO implements MyPageDAOInter{
-
-	@Autowired
-	SqlSessionTemplate sqlSession;
-
-	@Override
-	public List<NoticeVO> selectAllNotice(Map<String, Integer> pageMap) {
-		return sqlSession.selectList("kr.co.mlec.notice.selectAllNotice",pageMap);
-	}
-
-	@Override
-	public void insertNotice(NoticeVO noticeVO) {
-		sqlSession.insert("kr.co.mlec.notice.insertNotice", noticeVO);
-	}
-}
+//package kr.co.mlec.mypage;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.mybatis.spring.SqlSessionTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Repository;
+//
+//import kr.co.mlec.notice.NoticeVO;
+//
+//@Repository
+//public class MyPageDAO implements MyPageDAOInter{
+//
+//	@Autowired
+//	SqlSessionTemplate sqlSession;
+//
+//	@Override
+//	public List<NoticeVO> selectAllNotice(Map<String, Integer> pageMap) {
+//		return sqlSession.selectList("kr.co.mlec.notice.selectAllNotice",pageMap);
+//	}
+//
+//	@Override
+//	public void insertNotice(NoticeVO noticeVO) {
+//		sqlSession.insert("kr.co.mlec.notice.insertNotice", noticeVO);
+//	}
+//}
--- src/main/java/kr/co/mlec/mypage/MyPageDAOInter.java
+++ src/main/java/kr/co/mlec/mypage/MyPageDAOInter.java
@@ -1,12 +1,12 @@
-package kr.co.mlec.mypage;
-
-import java.util.List;
-import java.util.Map;
-
-import kr.co.mlec.notice.NoticeVO;
-
-public interface MyPageDAOInter {
-
-	public List<NoticeVO> selectAllNotice(Map<String, Integer> pageMap); 
-	public void insertNotice(NoticeVO noticeVO);
-}
+//package kr.co.mlec.mypage;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//import kr.co.mlec.notice.NoticeVO;
+//
+//public interface MyPageDAOInter {
+//
+//	public List<NoticeVO> selectAllNotice(Map<String, Integer> pageMap); 
+//	public void insertNotice(NoticeVO noticeVO);
+//}
