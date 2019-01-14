70848e784640e19c28890c76f5483fac3089d523
황세로 <hsro2228@naver.com>
2019-01-14 16:57:05 +0900

    메인수정

--- src/main/java/kr/co/mlec/diary/DiaryServiceInter.java
+++ src/main/java/kr/co/mlec/diary/DiaryServiceInter.java
@@ -13,6 +13,7 @@ public interface DiaryServiceInter {
 	public List<DiaryVO> selectFiveDiary(Map<String, Object> parameters);
 	public List<DiaryVO> selectSearchFiveDiary(Map<String, Object> parameters);
 	public void upView_cnt(int no);
+	public List<String> selectBestTag();
 	
 	
 }
